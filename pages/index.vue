06d057606f65f4b1cccd5945b987146c75576bc7
kuncismagic <kuncii404@gmail.com>
2022-11-10 09:02:36 +0700

    small changes

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,11 @@
     <v-col cols="8" class="frame-content px-6">
       <Products></Products>
     </v-col>
-    <v-col cols="4">ORDERS</v-col>
+    <v-col cols="4" class="frame-content white">
+      ORDERS
+    </v-col>
   </v-row>
 </template>
 
 <style scoped>
-  .frame-content{
-    height: 90vh;
-    overflow-y: auto;
-  }
 </style>
