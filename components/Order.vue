67446770ec17a264228fbf898a6e927d1b4d16d1
kuncismagic <kuncii404@gmail.com>
2022-11-18 08:03:16 +0700

    added Order components

---
+++ components/Order.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		{{items}}
+	</div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+	computed: {
+		...mapState('carts', {
+			items: 'items',
+		})
+	}
+}
+</script>
