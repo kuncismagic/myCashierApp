101cb23d1f3c6be72a00930abb8dc83fb5726ead
kuncismagic <kuncii404@gmail.com>
2022-11-18 07:56:21 +0700

    update quantity product

--- store/carts.js
+++ store/carts.js
@@ -8,11 +8,19 @@ export const mutations = {
 			id: id,
 			quantity: 1
 		})
+	},
+	incrementItem(state, id){
+		state.items.find(item => item.id === id).quantity++
 	}
 }
 
 export const actions = {
-	addToCart({commit}, id) {
-		commit('addItem', id)
+	addToCart({commit, state}, id) {
+		const found = state.items.find(item => item.id === id)
+		if (found){
+			commit('incrementItem', id)
+		}else {
+			commit('addItem', id)
+		}
 	}
 }
