78b55db4fefa0ee20dde6b8ebb0745038223e105
kuncismagic <kuncii404@gmail.com>
2022-11-18 08:51:33 +0700

    added Order function and prices

--- components/Order.vue
+++ components/Order.vue
@@ -1,17 +1,36 @@
 <template>
-	<div>
-		{{items}}
-	</div>
+	<v-row>
+		<v-col cols="12">
+			<h2>Order</h2>
+			<v-list>
+				<v-list-item v-for="(item, index) in cartItems"
+				:key="index">
+					<v-list-item-content>
+						<v-list-item-title>{{item.title}}</v-list-item-title>
+						<v-list-item-subtitle>{{currency(item.price)}}</v-list-item-subtitle>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-col>
+	</v-row>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex';
 
 export default {
+	methods: {
+		currency(value) {
+			return Intl.NumberFormat('en-US').format(value)
+		}
+	},
 	computed: {
 		...mapState('carts', {
 			items: 'items',
+		}),
+		...mapGetters('carts', {
+			cartItems: 'cartItems',
 		})
-	}
+	},
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,6 @@
       <Product></Product>
     </v-col>
     <v-col cols="4" class="frame-content white">
-      ORDERS
       <Order></Order>
     </v-col>
   </v-row>
